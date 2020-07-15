f1281ce2cc78f3e3112a0737505e6f025a0f6867
Juan Gamboa <juansimon.gr@gmail.com>
2020-07-15 18:53:13 -0500

    Added functional requirements

--- pswd_mgr.py
+++ pswd_mgr.py
@@ -1,10 +1,9 @@
-import os
-from sqlite3 import connect, OperationalError
+from sqlite3 import connect
 from time import ctime
 
 
 DATABASE = "credentials.db"
-MASTER_PASSWORD = "Juanita"
+MASTER_PASSWORD = "12345"
 
 
 def authentication(func):
@@ -25,32 +24,62 @@ def authentication(func):
     return wrapper
 
 
+def user_input():
+    user = input("Enter new username: ")
+    pswd = input("Enter new password: ")
+    date = ctime()
+    return (user, pswd, date)
+
+
 def create_table(c):
-    sql_command = '''
-                        CREATE TABLE IF NOT EXISTS credentials_table(
+    sql_command = '''CREATE TABLE IF NOT EXISTS credentials_table(
                             website TEXT PRIMARY KEY,
                             username TEXT NOT NULL,
                             password TEXT NOT NULL,
-                            creation_date DATE
-                            )
-                       '''
+                            mod_date DATE
+                            )'''
     c.execute(sql_command)
 
 
-def show_records(c):
-    sql_query = '''
-                SELECT * FROM credentials_table
-                '''
-    c.execute(sql_query)
-    print(c.fetchall())
+def show_records(c, website=None):
+    add = f"WHERE website = '{website}'" if website is not None else ''
+    sql_query = "SELECT * FROM credentials_table %s" % add
+    print(sql_query)
+    for record in c.execute(sql_query):
+        print(record)
 
 
-def insert_record(c, website, username, password):
-    creation_date = ctime()
-    sql_command = '''
-                    INSERT INTO credentials_table values (?, ?, ?, ?)
-                  '''
-    c.execute(sql_command, (website, username, password, creation_date))
+def insert_record(c):
+    website = input("Enter new website: ")
+
+    query = c.execute(f"SELECT * FROM credentials_table WHERE website='{website}'").fetchall()
+
+    if len(query) > 0:
+        update_record(c, website)
+    else:
+        username, password, mod_date = user_input()
+
+        sql_command = "INSERT INTO credentials_table values (?, ?, ?, ?)"
+        c.execute(sql_command, (website, username, password, mod_date))
+
+
+def update_record(c, web=None):
+    website = web if web is not None else input("Enter website to update: ")
+    username, password, mod_date = user_input()
+
+    sql_comand = '''UPDATE credentials_table
+                    SET username = ?,
+                        password = ?,
+                        mod_date = ?
+                    WHERE website = ?'''
+    c.execute(sql_comand, (username, password, mod_date, website))
+
+
+def delete_record(c):
+    website = input("Password record to be deleted: ")
+    sql_command = "DELETE FROM credentials_table WHERE website = '%s'" % website
+    c.execute(sql_command)
+    print(f"Password for {website} deleted successfully!")
 
 
 @authentication
@@ -59,8 +88,38 @@ def main():
     c = conn.cursor()
 
     create_table(c)
-    insert_record(c, 'coursera', 'juansimon.gr', 'Juanita24')
-    show_records(c)
+
+    while True:
+
+        print("\n***************** USER OPTIONS *****************\n",
+              "--- press 'i' -> create a new password record\n",
+              "--- press 'd' -> delete a password record\n",
+              "--- press 'u' -> update an existing record\n",
+              "--- press 's' -> display record(s)\n",
+              "--- press 'exit' -> exit the script\n",
+              "***********************************************\n")
+        option = input("-> ")
+
+        if option == 'i':
+            insert_record(c)
+            conn.commit()
+
+        elif option == 'd':
+            delete_record(c)
+            conn.commit()
+
+        elif option == 'u':
+            update_record(c)
+            conn.commit()
+
+        elif option == 's':
+            show_records(c)
+
+        elif option == 'exit':
+            break
+
+        else:
+            print("INVALID OPTION -> Please refer to available user options")
 
     conn.close()
 
